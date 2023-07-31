1200b5c5e9624cb81e2bfc28d205fd132c832792
Amirhosseinpourjon <131782746+am13ir84@users.noreply.github.com>
2023-07-31 17:58:28 +0330

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+let num = parseFloat(prompt("Enter a number: "));
+let nearest_num = Math.round(num);
+let log_num = Math.log(nearest_num);
+console.log("The nearest number is:", nearest_num);
+console.log("Log of the nearest number is:", log_num);
